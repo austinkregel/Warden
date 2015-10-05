62fbe918d7f23b52aa641985bbfdae51bfd44ca6
Austin Kregel <austinkregel@gmail.com>
2015-10-05 11:57:22 -0400

    Update composer dependancy, Update Routes.php style

--- src/Warden/Http/routes.php
+++ src/Warden/Http/routes.php
@@ -4,11 +4,10 @@
  * 
  */
 Route::group(['prefix' => 'warden', 'as' => 'warden::'], function () {
-  Route::get('{model}/manage/new',  ['as' => 'new-model',    'uses' => 'ModelController@getNewModel'  ]);
-  Route::post('{model}/manage',      ['as' => 'create-model', 'uses' => 'ModelController@createModel'  ]);
-  Route::get('{model}s/manage',     ['as' => 'models',       'uses' => 'ModelController@getModelList' ]);
-  Route::get('{model}/manage/{id}', ['as' => 'model',        'uses' => 'ModelController@getModel'     ]);
-  Route::delete('{model}/manage/{id}', ['as' => 'delete-model', 'uses' => 'ModelController@deleteModel'  ]);
-  Route::put('{model}/manage/{id}', ['as' => 'update-model', 'uses' => 'ModelController@updateModel'  ]);
-  
+  Route::get('{model}/manage/new', ['as' => 'new-model', 'uses' => 'ModelController@getNewModel']);
+  Route::post('{model}/manage', ['as' => 'create-model', 'uses' => 'ModelController@createModel']);
+  Route::get('{model}s/manage', ['as' => 'models', 'uses' => 'ModelController@getModelList']);
+  Route::get('{model}/manage/{id}', ['as' => 'model', 'uses' => 'ModelController@getModel']);
+  Route::delete('{model}/manage/{id}', ['as' => 'delete-model', 'uses' => 'ModelController@deleteModel']);
+  Route::put('{model}/manage/{id}', ['as' => 'update-model', 'uses' => 'ModelController@updateModel']);
 });
