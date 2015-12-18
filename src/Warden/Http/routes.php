c5a1f08d7d577a20447eb987dd03b6e95b2f1d08
Austin Kregel <austin@kbcomp.co>
2015-12-18 13:31:58 -0500

    Fix default page and route naming

--- src/Warden/Http/routes.php
+++ src/Warden/Http/routes.php
@@ -4,7 +4,11 @@
  *
  */
 Route::group(['prefix' => config('kregel.warden.route'), 'as' => 'warden::'], function () {
-    get('test/{route}', 'ModelController@parseRoute');
+
+    Route::get('/', function(){
+        return view('warden::base');
+    });
+
     Route::get('{model}/manage/new', ['as' => 'new-model', 'uses' => 'ModelController@getNewModel']);
     Route::get('{model}s/manage', ['as' => 'models', 'uses' => 'ModelController@getModelList']);
     Route::get('{model}/manage/{id}', ['as' => 'model', 'uses' => 'ModelController@getModel']);
