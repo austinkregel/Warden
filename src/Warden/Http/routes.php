e6c653b2714fc8f9a90be1b6bfeff7fd6e735a04
Austin Kregel <austinkregel@gmail.com>
2016-05-19 11:37:59 -0400

    Remove api routes

--- src/Warden/Http/routes.php
+++ src/Warden/Http/routes.php
@@ -9,24 +9,4 @@ Route::group(['prefix' => config('kregel.warden.route'), 'as' => 'warden::', 'mi
     Route::get('{model}/manage/new', ['as' => 'new-model', 'uses' => 'ModelController@getNewModel']);
     Route::get('{model}s/manage', ['as' => 'models', 'uses' => 'ModelController@getModelList']);
     Route::get('{model}/manage/{id}', ['as' => 'model', 'uses' => 'ModelController@getModel']);
-
-    // Api for basic things
-    Route::group(['prefix' => 'api/v1.0', 'as' => 'api.'], function () {
-        // Should retrieve almost all items...
-        Route::get('{model}s', ['as' => 'get-all', 'uses' => 'ApiController@getAllModels']);
-        // Should retrieve some items...
-        Route::get('{model}', ['as' => 'get-models', 'uses' => 'ApiController@getSomeModels']);
-        // Should get an item...
-        Route::get('{model}/{id}', ['as' => 'get-model', 'uses' => 'ApiController@findModel']);
-
-        // Should create a model using /warden/api/v1.0/user/
-        Route::post('{model}', ['as' => 'create-model', 'uses' => 'ApiController@postModel']);
-
-        // Should update a model.
-        Route::post('{model}/{id}', ['as' => 'update-model-post', 'uses' => 'ApiController@putModel']);
-        Route::put('{model}/{id}', ['as' => 'update-model', 'uses' => 'ApiController@putModel']);
-
-        // Should delete a model.
-        Route::delete('d/{model}/{id}', ['as' => 'delete-model', 'uses' => 'ApiController@deleteModel']);
-    });
 });
