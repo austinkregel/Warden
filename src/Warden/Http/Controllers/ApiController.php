b7b2cc7faa7c7566058e1a632db957c0c42b4450
Austin Kregel <austinkregel@gmail.com>
2016-08-09 16:18:34 -0400

    Refactor controller mainly focusing on the Put method

--- src/Warden/Http/Controllers/ApiController.php
+++ src/Warden/Http/Controllers/ApiController.php
@@ -2,26 +2,19 @@
 
 namespace Kregel\Warden\Http\Controllers;
 
+use Carbon\Carbon;
+use Closure;
+use FormModel;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
-use Closure;
+use Illuminate\Support\Collection;
+use Kregel\Warden\Warden;
 
 class ApiController extends Controller
 {
-    /**
-     * @param         $model_name
-     * @param Request $request
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function g($model_name, Request $request)
-    {
-        $this->checkParams(func_get_args()); // Filler bullshit.
-        $code = $request->ajax() ? 202 : 200;
-        $returnable = ['message' => 'Method success, but nothing was done.', 'code' => $code];
+    protected $model_name;
 
-        return response()->json($returnable, $code);
-    }
+    protected $model;
 
     /**
      * @param         $model_name
@@ -31,13 +24,14 @@ class ApiController extends Controller
      */
     public function getAllModels($model_name, Request $request)
     {
-        return $this->getSomeModels($model_name, $request, 50000);
+        $this->model_name = $model_name;
+        return $this->getSomeModels($request, 500);
     }
 
     /**
      * @param         $model_name
      * @param Request $request
-     * @param int     $paginate
+     * @param int $paginate
      *
      * @return \Illuminate\Http\JsonResponse
      */
@@ -45,23 +39,8 @@ class ApiController extends Controller
     {
         $this->checkParams(func_get_args());
         $model = $this->findModel($model_name);
-        $field_names = $this->getFields($model);
-        $all = $model::paginate($paginate);
-        $i = 0;
-        foreach ($all as $model) {
-            foreach ($field_names as $f) {
-                if (!in_array($f, $model->getHidden())) {
-                    $returnable[$model_name.'s'][$i] = $model->toArray();
-                }
-            }
-            ++$i;
-        }
-        if (empty($returnable)) {
-            $returnable = [];
-        }
         $status = $request->ajax() ? 202 : 200;
-
-        return response()->json($returnable, $status);
+        return response()->json($model->paginate($paginate), $status);
     }
 
     /**
@@ -72,25 +51,11 @@ class ApiController extends Controller
      */
     public function findModel($model_name, $id = null)
     {
-        $model = config('kregel.warden.models.'.$model_name.'.model');
+        $model = config('kregel.warden.models.' . $model_name . '.model');
         if (empty($id) | !is_numeric($id)) {
             return new $model();
         }
-
         return $model::find($id);
-    }
-
-    /**
-     * @param Model $model
-     *
-     * @return array
-     */
-    private function getFields(Model $model)
-    {
-        $field_names = !empty($model->getVisible()) ? $model->getVisible() : $model->getFillable();
-        $dates = !empty($model->getDates()) ? $model->getDates() : [];
-
-        return array_merge($field_names, $dates);
     }
 
     /**
@@ -104,83 +69,68 @@ class ApiController extends Controller
         $this->checkParams(func_get_args());
         $model = $this->findModel($model_name);
         if (empty($model)) {
-            if ($request->ajax()) {
-                return response()->json(['message' => 'No resource found!', 'code' => 404], 404);
-            }
-            return response(redirect('404'), 302);
+            return $this->emptyModel($request);
         }
         // Need a way to validate the input for the model. If we then can not find any
         // way to validate the inputs then we might have some un-wanted inputs from
         // some of the users. We probably won't need to worry about validations.
-        $input = $this->clearInput([
-                'uuid' => $this->generateUUID(),
+        $input = Warden::clearInput([
+                'uuid' => Warden::generateUUID(),
             ] + $request->all());
 
         $model->fill($input);
         if (!empty($model->password)) {
             $model->password = bcrypt($model->password);
         }
-        $inputs = $model->getFillable();
-        $relations = config('kregel.warden.models.'.$model_name.'.relations');
-        if (!empty($relations)) {
-            foreach ($input as $k => $i) {
-                if (in_array($k, $relations) || !(empty($relations[$k]))) { // Check if there is a relation
-                    // if it's in_array, it's not a closure, just have to sync. Otherwise it's a closure
-                    // And we will have to call the closure and pass through the need model to it.
-                    $model->$k()->sync($i);
-                    $update_event = config('kregel.warden.models.' . $model_name . '.relations.' . $k . '.new');
-                    if ($update_event instanceof Closure) {
-                        $update_event($model);
-                    }
+
+        // Update a relationship.
+        foreach ($input as $k => $i) {
+            // If there are no relations for this point just break.
+            if (empty($relations = FormModel::using('plain')->getRelationalDataAndModels($model, $k))) {
+                break;
+            }
+
+            if (in_array($k, $relations) || !(empty($relations[$k]))) { // Check if there is a relation
+                // if it's in_array, it's not a closure, just have to sync. Otherwise it's a closure
+                // And we will have to call the closure and pass through the need model to it.
+                $model->$k()->sync($i);
+                $update_event = config('kregel.warden.models.' . $model_name . '.relations.' . $k . '.new');
+                if ($update_event instanceof Closure) {
+                    $update_event($model);
                 }
             }
         }
-        $saved = $model->save();
+        return $this->modelHasBeenSaved($model->save(), 'created', $request);
+    }
+
+    /**
+     * This method handles how submitted quests are handle, the main related methods for it are
+     * POST and
+     *
+     * @param $saved
+     * @param $request
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
+     */
+    protected function modelHasBeenSaved($saved, $type, $request)
+    {
         if (!$saved) {
-            return response()->json(['message' => 'Failed to created resource', 'code' => 422], 422);
+            return response()->json(['message' => 'Failed to ' . trim($type, 'ed') . ' resource', 'code' => 422], 422);
         }
         $status = $request->ajax() ? 202 : 200;
         if ($request->ajax()) {
-            return response()->json(['message' => 'Successfully created resource', 'code' => $status], $status);
+            return response()->json(['message' => 'Successfully ' . $type . ' resource', 'code' => $status], $status);
         }
         if ($request->has('_redirect')) {
             // Remove the base part of the url, and just grab the tail end of the desired redirect, that way the
             // User can't be redirected away from your website.
-            return $this->returnRedirect('Successfully created resource', $request);
+            return $this->returnRedirect('Successfully ' . $type . ' resource', $request);
         }
 
-        return redirect()->back()->with(['message' => 'Successfully created resource']);
-    }
-
-    public function clearInput($input)
-    {
-        if (is_array($input)) {
-            foreach ($input as $key => $value) {
-                if (!isset($value) || $value === '') {
-                    unset($input[$key]);
-                }
-            }
-
-            return $input;
-        }
-
-        return $input;
-    }
-
-    /**
-     * @return string
-     */
-    public function generateUUID()
-    {
-        $data = openssl_random_pseudo_bytes(16);
-        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
-        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
-        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+        return redirect()->back()->with(['message' => 'Successfully ' . $type . ' resource']);
     }
 
     private function returnRedirect($msg, Request $request)
     {
-
         // Remove the base part of the url, and just grab the tail end of the desired redirect, that way the
         // User can't be redirected away from your website.
         $tmp = explode('/', preg_replace('/^(http|https):\/\//', '', $request->get('_redirect')));
@@ -200,61 +150,71 @@ class ApiController extends Controller
     {
         $this->checkParams(func_get_args());
         $model = $this->findModel($model_name, $id);
-        if (empty($model) && $request->ajax()) {
-            return response()->json(['message' => 'No resource found!', 'code' => 404], 404);
-        } else {
-            if (empty($model)) {
-                return $this->returnRedirect('No resource found!', $request);
-            }
+        if (empty($model)) {
+            return $this->emptyModel($request);
         }
-        $input = $this->clearInput($request->all()); // Remove the empty values.
+
+        $input = collect(Warden::clearInput($request->all())); // Remove the empty values.
         $this->validatePut($input, $model, $model_name); // Remove any values that are the same
 
-        if (collect($input)->isEmpty()) { // if the input is empty,
+        if ($input->isEmpty()) { // if the input is empty,
             return response()->json(['message' => 'Nothing to update for resource', 'code' => 205], 205);
         }
 
-        $input = collect($model->toArray())->merge($input)->toArray();
+        // Since wardenable messes with our normal method of just collect($model)
+        // So we have to map the fillable to a new model
+        $relationships = $input->filter(function ($value){
+            return is_array($value);
+        });
+        // Assume that anything not in an array isn't a relationship.
+        $not_relationships = $input->filter(function($value){
+            return !is_array($value);
+        });
 
-        $model->fill($input);
+        /**
+         * Here we need to calculate the actual values of our model. Since we're
+         * just updating what we can then we only need the fillable from the model
+         *
+         * If the model has the Wardenable Trait then it skews the results with collections
+         * and it also removes the _id's from the model and trims the value.
+         *
+         * If it has the Wardenable Trait, calculate the real values by flatMaping the values
+         * of the getFillable array to the model it's self.
+         */
+        if (method_exists($model, 'getWarden')) {
+            $model_array = collect($model->getFillable())->flatMap(function ($value) use ($model) {
+                // If the value is an instance of carbon it will throw an 'Array to String' exception.
+                if ($model->$value instanceof Carbon)
+                    // Convert the string to
+                    return [$value => $model->$value->__toString()];
+                return [$value => $model->$value];
+            });
+        } else {
+            // Since the model doesn't have getWarden then it doesn't have warden able.
+            $model_array = $model->toArray();
+        }
 
-        // Removed the file handling code, as put requested files come in via stdin.
-        // Just use a post request unless you NEED the put request.
+        // Before filling the model we need to convert all date time stamps into normal timestamps
+        $dates = collect($not_relationships)->filter(function($val, $key) use ($model){
+            return in_array($key, $model->getDates());
+        })->flatMap(function($value, $key) use ($model){
+            if(count(explode(' ', $value)) > 1)
+                return [$key => Carbon::createFromFormat('Y-m-d H:i:s', $value)];
+            return [$key => Carbon::createFromFormat('Y-m-d', $value)];
+        });
 
-        $relations = config('kregel.warden.models.'.$model_name.'.relations');
-        if (!empty($relations)) {
-            foreach ($input as $k => $i) {
+        // Fill the differences between the current values and the new input.
+        $model->fill($not_relationships->merge($dates)->diff($model_array)->toArray());
 
-                if (in_array($k, $relations) || !(empty($relations[$k]))) { // Check if there is a relation
-                    // if it's in_array, it's not a closure, just have to sync. Otherwise it's a closure
-                    // And we will have to call the closure and pass through the need model to it.
-                    $users = [];
-                    foreach($model->$k as $user){
-                        $users[] = $user->id;
-                    }
-                    if($users != $i) {
-                        $model->$k()->sync($i);
-                        $update_event = config('kregel.warden.models.' . $model_name . '.relations.' . $k . '.update');
-                        if ($update_event instanceof Closure) {
-                            $update_event($model);
-                        }
-                    }
-                }
+        // Make sure the array
+        if(!$relationships->isEmpty()) {
+            foreach ($relationships as $many_relation => $values) {
+                $model->$many_relation()->sync($values, false);
             }
         }
 
         $saved = $model->save();
-        if (!$saved) {
-            return response()->json(['message' => 'Failed to updated resource', 'code' => 422], 422);
-        }
-        if ($request->has('_redirect')) {
-            // Remove the base part of the url, and just grab the tail end of the desired redirect, that way the
-            // User can't be redirected away from your website.
-            return $this->returnRedirect('Successfully updated resource', $request);
-        }
-        $status = $request->ajax() ? 202 : 200;
-
-        return response()->json(['message' => 'Successfully updated resource', 'code' => $status], $status);
+        return $this->modelHasBeenSaved($saved, 'updated', $request);
     }
 
     /**
@@ -264,51 +224,41 @@ class ApiController extends Controller
      *
      * @param $input
      * @param $model
+     * @return Collection
      */
-    public function validatePut(&$input, $model, $model_name)
+    public function validatePut($input, $model, $model_name)
     {
-        foreach ($input as $k => $v) {
-            if (empty($v) || $k === '_token') {
-                unset($input[$k]);
+
+         return collect($input)->filter(function ($value, $key) use ($model, $input) {
+//            // Remove _token
+            if (!isset($value) || $key === '_token') {
+                return false;
             }
-            if (!empty($model->$k)) {
-                if ($model->$k === $v) {
-                    unset($input[$k]);
+            //Remove values that are the same.
+            if (isset($model->$key)) {
+                if ($model->$key === $value) {
+                    return false;
                 }
             }
-            if ($this->doesModelRelate($model, $k, $v)) {
-                unset($input[$k]);
+            // Is there a relation? If there is, unset it.
+            if ($this->doesModelRelate($model, $key, $value)) {
+                return false;
             }
-            if (((stripos($k, 'password') !== false) || (stripos($k, 'passwd') !== false)) && !empty($model->$k)) {
-                if (\Hash::check($v, $model->$k)) {
-                    unset($input[$k]);
+            // If there is a password field,
+            if (((stripos($key, 'password') !== false) || (stripos($key, 'passwd') !== false)) && !empty($model->$key)) {
+                if (\Hash::check($value, $model->$key)) {
+                    return false;
                 } else {
-                    $user_model = config('auth.model');
+                    $user_model = config('kregel.warden.models.user.model');
                     $user = new $user_model();
                     if (empty($user->hashable)) {
-                        $input[$k] = bcrypt($v);
-                    }
-                }
-            }
-        }
-    }
-
-    private function doesModelRelate(Model $model, $relation, $objects)
-    {
-        $relations = config('kregel.warden.models.'.$relation.'.relations');
-        if ($relations !== null && in_array($relation, $relations)) {
-            if (is_array($objects)) {
-                foreach ($objects as $k => $v) {
-                    if ($model->$relation->contains($v)) {
+                        $input[$key] = bcrypt($value);
                         return true;
                     }
                 }
-            } else {
-                return (bool) $model->$relation->contains($objects);
             }
-        }
-
-        return false;
+            return false;
+        });
     }
 
     /**
@@ -328,12 +278,12 @@ class ApiController extends Controller
         $model = $this->findModel($model_name, $id);
         if (empty($model->id)) {
             if ($model = $model::withTrashed()->whereId($id)->first()) {
-                $relations = config('kregel.warden.models.'.$model_name.'.relations');
+                $relations = config('kregel.warden.models.' . $model_name . '.relations');
                 foreach ($relations as $rel) {
                     $model->$rel()->forceDelete();
 
-                    $update_event = config('kregel.warden.models.'.$model_name.'.relations.'.$rel.'.delete');
-                    if($update_event instanceof Closure){
+                    $update_event = config('kregel.warden.models.' . $model_name . '.relations.' . $rel . '.delete');
+                    if ($update_event instanceof Closure) {
                         $update_event($model);
                     }
                 }
@@ -352,5 +302,10 @@ class ApiController extends Controller
         }
 
         return response()->json(['message' => 'Successfully deleted resource', 'code' => $status], $status);
+    }
+
+    private function doesModelRelate(Model $model, $relation, $objects)
+    {
+        return FormModel::using('plain')->getRelationalDataAndModels($model, $relation) !== null;
     }
 }
